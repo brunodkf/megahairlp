2613c1d452eb4a1b4294aed2fbe9583350f4a54b
Bruno Furtado <bsouzacc@gmail.com>
2024-09-11 12:47:32 -0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+const midias = document.querySelectorAll('video');
+const videos = [];
+
+midias.forEach(e => {
+    e.addEventListener('click', (event) => {
+        console.log(event)
+        if(event.target.paused){
+            e.play();
+        }else{
+            e.pause();
+            event.target.currentTime = 0;
+        }
+    })
+})
