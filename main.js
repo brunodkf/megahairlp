bb7f0eef2c48ea5bf511d2e2cfecdb1153b3bc29
Bruno Furtado <bsouzacc@gmail.com>
2024-09-11 16:14:50 -0300

    update

--- main.js
+++ main.js
@@ -13,3 +13,50 @@ midias.forEach(e => {
         }
     })
 })
+
+
+
+// Animação dos números
+
+const contadorContainer = document.querySelector('[data-target="contador"]');
+const contadorNumeros = document.querySelectorAll('.contador__numero');
+const diaAteEvento = 50;
+const date = new Date();
+const diasFaltam = diaAteEvento - date.getDate()
+
+
+console.log(diaAteEvento - date.getDate())
+
+const ativarContador = () => {
+    contadorNumeros.forEach((e) => {
+
+        function increment(i, max) {
+            if (i > max) return;
+            setTimeout(function () {
+                e.innerText = i;
+                increment(i + 1, max);
+            }, 50)
+        }
+
+        if(e.classList.contains('dias')){
+            increment(date.getDate(), diasFaltam)
+        }else if(e.classList.contains('horas')){
+            increment(0, 19);
+        }else if(e.classList.contains('min')){
+            increment(0, 26);
+        }
+        
+       
+    })
+}
+
+
+const observer = new IntersectionObserver(e => {
+    if (e[0].isIntersecting) {
+        ativarContador();;
+    }
+}, {
+    threshold: 0
+})
+
+observer.observe(contadorContainer)
